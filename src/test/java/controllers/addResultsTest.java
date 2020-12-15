9dc0a6f457c4145ef1be465c9022bca671c8a0cd
andreashovland <andreashovland96@gmail.com>
2020-12-15 16:04:34 +0100

    Stylet editAthletes

---
+++ src/test/java/controllers/addResultsTest.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class addResultsTest extends addResults {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testDoPost() {
+    }
+}
